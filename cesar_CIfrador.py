2871d4bfdc6ef9291ece429058c00ae202042d61
ktorres63 <ktorresaro@unsa.edu.pe>
2023-06-29 22:35:25 -0500

    add cesar and vignere

---
+++ cesar_CIfrador.py
@@ -0,0 +1,27 @@
+#CIFRADOR
+archivo = "testCifrar.txt"
+#archivo = input("ingrese el nombre del archivo: ")
+try:
+    file = open(archivo,'r')
+    alphaC = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+
+    cont = file.read().upper()
+    #desp = int(input("ingrese desplazamiento: "))
+    desp = 3
+    textcifr=""
+
+    #C = M +3 mod 27
+    
+    for c in cont:
+        if c in alphaC:
+            indice = (alphaC.index(c) + desp) % 27
+            textcifr += alphaC[indice]
+        else:
+            continue
+
+
+    print(cont)
+    print(textcifr)
+
+except FileNotFoundError:
+    print("El archivo no existe")
---
+++ vignere_Cifrador.py
@@ -0,0 +1,94 @@
+def preProcAlpha(text):  # 27 characters
+    textP = ""
+    for c in text:
+        if c == " " or c == ",":
+            continue
+        elif c == "Á":
+            textP += "A"
+        elif c == "É":
+            textP += "E"
+        elif c == "Í":
+            textP += "I"
+        elif c == "Ó":
+            textP += "O"
+        elif c == "Ú":
+            textP += "U"
+        else:
+            textP += c
+
+    return textP
+
+def preProcASCII(text): #ASCII characters
+    textP = ""
+    for c in text:
+        textP += c
+    return textP
+
+
+def ciphASCII(key, text):
+    txtCiph = ""
+
+    #Ci=(Mi+Ki)%191
+    for i in range(len(text)):
+        m = ord(text[i])
+        k = ord(key[i])
+
+        tmp = (m+k) % 191
+        txtCiph+=chr(tmp)
+    return txtCiph
+
+def ciphVS(key, text):
+    alpha = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+    txtCiph = ""
+
+    #Ci=(Mi+Ki)%27
+    for i in range(len(text)):
+        m = alpha.index(text[i])
+        k = alpha.index(key[i])
+        tmp = (m+k) % 27
+        txtCiph+=alpha[tmp]
+    return txtCiph
+
+
+def nKey(key, sizeW):
+    nKey = ""
+    for i in range(sizeW):
+        nKey += key[i % len(key)]
+    return nKey
+
+
+# archivo = input("ingrese el nombre del archivo: ")
+archivo = "testCifrar.txt"
+try:
+    file = open(archivo, 'r')
+    #contRaw = file.read()
+    cont = file.read()
+    contUpp = cont.upper()
+
+    alpha = input("seleccione (1) para cifrar en alfabeto de 27 o (2) para cifrar en alfabeto de 191: ")
+    #alpha = "1"
+
+    # key = input("ingresa la clave: ").upper()
+    key = "MEZCLADOR"
+    match alpha:
+        case "1":  # 27 letras
+            print(cont)
+            tProc = preProcAlpha(contUpp)
+            nkey = nKey(key, len(tProc))
+
+            print(ciphVS(nkey, tProc))
+
+        case "2":
+
+            tProc = preProcASCII(cont)
+            nkey = nKey(key, len(tProc))
+            print(tProc)
+            print(nkey)
+
+            print(ciphASCII(nkey, tProc))
+        case _:
+            print("ingrese un numero valido")
+
+
+except FileNotFoundError:
+    print("El archivo no existe")
